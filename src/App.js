261eb042ac9ef4152eb1ce9f23c011fba7238335
tuyenphann <tuyenphan@kms-technology.com>
2022-07-20 21:13:42 +0700

    Generate conflicts

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React more!
+          Learn more!
         </a>
         <br />
         <div>projectId: {firebaseConfig.projectId}</div>
