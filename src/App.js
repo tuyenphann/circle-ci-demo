1cb51f52fc0b12d708b37002fcc8ede4126264f6
tuyenphann <tuyenphan@kms-technology.com>
2022-07-20 21:16:32 +0700

    Generate conflicts

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn more!
+          Learn GitHub Actions more!
         </a>
         <br />
         <div>test1 env</div>
