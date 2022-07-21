546b1c6d32f924ff7ed1da9bcd0fc19b9e6f65ea
tuyenphann <tuyenphan@kms-technology.com>
2022-07-21 13:11:09 +0700

    Make changes

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React more!!!!!!!?
+          Learn React more!!!!!!!??
         </a>
         <br />
         <div>projectId: {firebaseConfig.projectId}</div>
